0e7d3219394322e370b8815959d70b573b30b54d
vkmercury7 <153854218+vkmercury7@users.noreply.github.com>
2026-09-15 16:24:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
     tanstackStart(),
     react(),
   ],
